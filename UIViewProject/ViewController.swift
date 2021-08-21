31432112bd441dcfc29d23cefb18b0ef5466ed36
Andrey Sinetskiy <siniy.85@mail.ru>
2021-08-21 07:41:33 +0300

    Initial Commit

---
+++ UIViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIViewProject
+//
+//  Created by 1 on 21.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
